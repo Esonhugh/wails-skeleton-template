d00cbdacbfb5c0f87809fb3a8de77b331fd51ba7
Esonhugh <esonhughoutside@gmail.com>
2023-09-03 16:54:48 +0800

    init project

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Configure for popup
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl"> Untitled </strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,28 @@
+// @ts-check
+// 1. Import the Skeleton plugin
+const { skeleton } = require('@skeletonlabs/tw-plugin');
+import forms from '@tailwindcss/forms'
+//import { join } from 'path';
+//import skeleton from '@skeletonlabs/tw-plugin/skeleton.cjs'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		require('path').join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		skeleton({
+			themes: { preset: [ "skeleton" ] }
+		}),
+	]
+}
